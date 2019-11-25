05c204801f446d263f357b89f65dbef2b75e800c
Qu <Quan0027@elec.techcollege.dk>
2019-11-25 10:43:17 +0100

    Constructors - Creating a constructor

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -8,21 +8,15 @@ namespace CSharpIntermediate
 {
     class Program
     {
-        public class Person
+        public class Customer
         {
+            public int Id;
             public string Name;
-
-            public void Introduce(string to)
-            {
-                Console.WriteLine("Hi {0}, I am {1}", to, Name);
-            }
+            
         }
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.Name = "John";
-
-            person.Introduce("Mosh");
+           
 
 
             Console.ReadLine();
