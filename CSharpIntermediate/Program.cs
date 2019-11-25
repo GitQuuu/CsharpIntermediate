7a2f1f04f6714a4474ed5b85a7ddf7ab5f5e0435
Qu <Quan0027@elec.techcollege.dk>
2019-11-25 10:20:40 +0100

    Classes

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -8,8 +8,26 @@ namespace CSharpIntermediate
 {
     class Program
     {
+        public class Person
+        {
+            public string Name;
+
+            public void Introduce(string to)
+            {
+                Console.WriteLine("Hi {0}, I am {1}", to, Name);
+            }
+        }
         static void Main(string[] args)
         {
+            Person person = new Person();
+            person.Name = "John";
+
+            person.Introduce("Mosh");
+
+
+            Console.ReadLine();
         }
+
+
     }
 }
